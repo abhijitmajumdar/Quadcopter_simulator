026a2be2119fc9d933153bfebbeeb5bdfea867a6
Kai Ergin <kergin2016@gmail.com>
2018-07-16 14:30:48 -0400

    Added new controller without PID

--- controller.py
+++ controller.py
@@ -3,6 +3,60 @@ import math
 import time
 import threading
 
+class Controller_AI():
+    def __init__(self, get_state, get_time, actuate_motors, params, quad_identifier):
+        self.quad_identifier = quad_identifier
+        self.actuate_motors = actuate_motors
+        self.get_state = get_state
+        self.get_time = get_time
+        self.MOTOR_LIMITS = params['Motor_limits']
+        self.TILT_LIMITS = [(params['Tilt_limits'][0]/180.0)*3.14,(params['Tilt_limits'][1]/180.0)*3.14]
+        self.YAW_CONTROL_LIMITS = params['Yaw_Control_Limits']
+        self.Z_LIMITS = [self.MOTOR_LIMITS[0]+params['Z_XY_offset'],self.MOTOR_LIMITS[1]-params['Z_XY_offset']]
+        self.LINEAR_TO_ANGULAR_SCALER = params['Linear_To_Angular_Scaler']
+        self.YAW_RATE_SCALER = params['Yaw_Rate_Scaler']
+        self.thread_object = None
+        self.target = [0,0,0]
+        self.yaw_target = 0.0
+        self.run = True
+
+    def wrap_angle(self,val):
+        return( ( val + np.pi) % (2 * np.pi ) - np.pi )
+
+    def update(self):
+        [dest_x,dest_y,dest_z] = self.target
+        [x,y,z,x_dot,y_dot,z_dot,theta,phi,gamma,theta_dot,phi_dot,gamma_dot] = self.get_state(self.quad_identifier)
+        reward = -(theta_dot + phi_dot + gamma_dot)**2
+        m1 = 0
+        m2 = 0
+        m3 = 0
+        m4 = 0
+        M = np.clip([m1,m2,m3,m4],self.MOTOR_LIMITS[0],self.MOTOR_LIMITS[1])
+        self.actuate_motors(self.quad_identifier,M)
+
+    def update_target(self,target):
+        self.target = target
+
+    def update_yaw_target(self,target):
+        self.yaw_target = self.wrap_angle(target)
+
+    def thread_run(self,update_rate,time_scaling):
+        update_rate = update_rate*time_scaling
+        last_update = self.get_time()
+        while(self.run==True):
+            time.sleep(0)
+            self.time = self.get_time()
+            if (self.time - last_update).total_seconds() > update_rate:
+                self.update()
+                last_update = self.time
+
+    def start_thread(self,update_rate=0.005,time_scaling=1):
+        self.thread_object = threading.Thread(target=self.thread_run,args=(update_rate,time_scaling),daemon=True)
+        self.thread_object.start()
+
+    def stop_thread(self):
+        self.run = False
+
 class Controller_PID_Point2Point():
     def __init__(self, get_state, get_time, actuate_motors, params, quad_identifier):
         self.quad_identifier = quad_identifier
--- quad_sim.py
+++ quad_sim.py
@@ -9,6 +9,44 @@ QUAD_DYNAMICS_UPDATE = 0.002 # seconds
 CONTROLLER_DYNAMICS_UPDATE = 0.005 # seconds
 run = True
 
+def Single_AI():
+    # Set goals to go to
+    GOALS = [(1,1,5)]
+    YAWS = [0]
+    # Define the quadcopters
+    QUADCOPTER={'q1':{'position':[1,0,4],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2}}
+    # Controller parameters
+    CONTROLLER_PARAMETERS = {'Motor_limits':[4000,9000],
+                        'Tilt_limits':[-10,10],
+                        'Yaw_Control_Limits':[-900,900],
+                        'Z_XY_offset':500,
+                        'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
+                        'Linear_To_Angular_Scaler':[1,1,0],
+                        'Yaw_Rate_Scaler':0.18,
+                        'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
+                        }
+
+    # Catch Ctrl+C to stop threads
+    signal.signal(signal.SIGINT, signal_handler)
+    # Make objects for quadcopter, gui and controller
+    quad = quadcopter.Quadcopter(QUADCOPTER)
+    gui_object = gui.GUI(quads=QUADCOPTER)
+    ctrl = controller.Controller_AI(quad.get_state,quad.get_time,quad.set_motor_speeds,params=CONTROLLER_PARAMETERS,quad_identifier='q1')
+    # Start the threads
+    quad.start_thread(dt=QUAD_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
+    ctrl.start_thread(update_rate=CONTROLLER_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
+    # Update the GUI while switching between destination poitions
+    while(run==True):
+        for goal,y in zip(GOALS,YAWS):
+            ctrl.update_target(goal)
+            ctrl.update_yaw_target(y)
+            for i in range(300):
+                gui_object.quads['q1']['position'] = quad.get_position('q1')
+                gui_object.quads['q1']['orientation'] = quad.get_orientation('q1')
+                gui_object.update()
+    quad.stop_thread()
+    ctrl.stop_thread()
+
 def Single_Point2Point():
     # Set goals to go to
     GOALS = [(1,1,2),(1,-1,4),(-1,-1,2),(-1,1,4)]
@@ -160,3 +198,5 @@ if __name__ == "__main__":
         Multi_Point2Point()
     elif args.sim == 'single_velocity':
         Single_Velocity()
+    elif args.sim == 'single_ai':
+        Single_AI()
